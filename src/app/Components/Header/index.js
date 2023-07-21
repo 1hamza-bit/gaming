e798101524455588c54a8f7fe52f86f4bc8444bd
unknown <hamza.tahir@ginkgoretail.com>
2023-07-21 10:29:46 +0500

    menu

--- src/app/Components/Header/index.js
+++ src/app/Components/Header/index.js
@@ -2,9 +2,13 @@
 // import styles from "./index.module.scss"
 'use client'
 import styled from "@emotion/styled";
-import { AppBar, Box, Button, Container, Drawer, List, ListItem, ListItemText, Slide, Toolbar, Typography, makeStyles } from "@mui/material";
+import { AppBar, Box, Button, Container, Drawer, List, ListItem, ListItemIcon, ListItemText, Slide, Toolbar, Typography, makeStyles } from "@mui/material";
 import { useState } from "react";
 import styles from './index.module.scss'
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import WorkIcon from '@mui/icons-material/Work';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
 
 function Header()  {
 const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +17,7 @@ const [isOpen, setIsOpen] = useState(false);
 
 const StyledDrawer = styled(Drawer)({
     minWidth: 250,
-    width: 340,
+    width: 270,
     flexShrink: 0,
   });
   
@@ -41,7 +45,7 @@ const toggleDrawer =() => {
                             </div>
                             </button>
                             <Typography className="ml40p fantasy" variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                News
+                                Mob Studios
                             </Typography>
                             <Button color="inherit">Login</Button>
                         </Toolbar>
@@ -60,17 +64,29 @@ const toggleDrawer =() => {
           <p>hamza</p>
           <Container className={styles.container}>
       <List className={styles.list}>
-        <ListItem className={styles.item}>
-          <ListItemText primary="Item 1" />
+      <ListItem className={styles.item}>
+          <ListItemIcon className={styles.icon}>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
         </ListItem>
         <ListItem className={styles.item}>
-          <ListItemText primary="Item 2" />
+          <ListItemIcon className={styles.icon}>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary="About" />
         </ListItem>
         <ListItem className={styles.item}>
-          <ListItemText primary="Item 3" />
+          <ListItemIcon className={styles.icon}>
+            <WorkIcon />
+          </ListItemIcon>
+          <ListItemText primary="Portfolio" />
         </ListItem>
         <ListItem className={styles.item}>
-          <ListItemText primary="Item 4" />
+          <ListItemIcon className={styles.icon}>
+            <ContactMailIcon />
+          </ListItemIcon>
+          <ListItemText primary="Contact" />
         </ListItem>
       </List>
     </Container>

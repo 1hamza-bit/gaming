831eda0bace29cdb34ac0682e5f5ac8178a59d76
1hamza-bit <you@example.com>
2023-08-01 17:58:31 +0100

    scrol

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+// import ScrollProgressBar from '../src/app/Components/ScrollProgressBar'
+import ScrollProgressBar from '../Components/ScrollProgressBar'
+import Abouts from '../Components/About'
+// import Hero from './Components/Home'
+
+
+export default function Contact() {
+  // useClient(); // Marking the parent component as a Client Component
+
+  return (
+    <>  
+    <ScrollProgressBar />
+      <Abouts />
+      </>
+
+  )
+}
